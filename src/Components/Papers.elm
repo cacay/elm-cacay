9620ff2491c8fbd5dbf87d1d3c7fa62a90995d04
Cosku Acay <coskuacay@gmail.com>
2016-09-11 23:17:05 -0700

    Add papers and profile picture

--- src/Components/Papers.elm
+++ src/Components/Papers.elm
@@ -1,7 +1,10 @@
 module Components.Papers exposing
   ( Model
-  , Paper (..)
   , Msg
+  , Paper
+  , Person
+  , Conference
+  , Link
   , init
   , update
   , subscriptions
@@ -11,7 +14,6 @@ module Components.Papers exposing
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-import String
 import Void
 
 
@@ -21,13 +23,35 @@ type Model =
   Model
 
 
-type Paper =
-  Paper
-
-
 init : Model
 init =
   Model
+
+
+type alias Paper =
+  { title : String
+  , authors : List Person
+  , conference : Conference
+  , link : Maybe Link
+  , extra : List (String, Link)
+  }
+
+
+type alias Person =
+  { name : String
+  , link : Maybe Link
+  }
+
+
+type alias Conference =
+  { name : String
+  , link : Maybe Link
+  , date : String
+  }
+
+
+type alias Link =
+  String
 
 
 -- UPDATE
@@ -52,7 +76,55 @@ subscriptions model =
 
 view : List Paper -> Model -> Html Msg
 view papers Model =
-  div []
-    []
+  div [ class "ui compact items" ]
+    ( List.map viewPaper papers )
 
+
+viewPaper : Paper -> Html msg
+viewPaper paper =
+  div [class "item"]
+    [ div [ class "content" ]
+        [ viewWithLink [ class "header" ] paper.title paper.link
+        , div [ class "description" ]
+            [ div []
+                ( List.map viewPerson paper.authors |> List.intersperse (text ", ") )
+            , viewConference paper.conference
+            ]
+        , div [ class "extra" ]
+            ( List.map viewExtra paper.extra )
+        ]
+    ]
+
+
+viewPerson : Person -> Html msg
+viewPerson person =
+  viewWithLink [] person.name person.link
+
+
+viewConference : Conference -> Html msg
+viewConference conference =
+  span []
+    [ viewWithLink [] conference.name conference.link
+    , text ", "
+    , text conference.date
+    ]
+
+
+viewExtra : (String, Link) -> Html msg
+viewExtra (s, link) =
+  span []
+    [ text "["
+    , viewWithLink [] s (Just link)
+    , text "]"
+    ]
+
+
+viewWithLink : List (Attribute msg) -> String -> Maybe Link -> Html msg
+viewWithLink attr s link =
+  case link of
+    Nothing ->
+      span attr [ text s ]
+
+    Just link' ->
+      a (attr ++ [ href link', target "_blank" ]) [ text s ]
 
