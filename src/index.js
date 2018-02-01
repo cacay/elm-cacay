46393999115a6e4dc27650358535347074990168
Cosku Acay <Acay@Josh.local>
2018-02-01 16:56:21 -0500

    deploy 71040e29f190a32aad648b3074fee6953fb1232a

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// pull in desired CSS/SASS files
-import './styles/main.less'
-
-
-// inject bundled Elm app into div#main
-import Elm from './Main'
-var app = Elm.Main.embed( document.getElementById( 'main' ) )
-
