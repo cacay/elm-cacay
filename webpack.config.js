357b4087a6c37a79b817b227cfbbb66366a37087
Cosku Acay <coskuacay@gmail.com>
2018-02-03 15:05:46 -0500

    Improve webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-var path                = require( 'path' );
-var webpack             = require( 'webpack' );
-var merge               = require( 'webpack-merge' );
-var HtmlWebpackPlugin   = require( 'html-webpack-plugin' );
-var autoprefixer        = require( 'autoprefixer' );
-var ExtractTextPlugin   = require( 'extract-text-webpack-plugin' );
-var CopyWebpackPlugin   = require( 'copy-webpack-plugin' );
+const path                  = require('path');
+const webpack               = require('webpack');
+const merge                 = require('webpack-merge');
+const HtmlWebpackPlugin     = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const ExtractTextPlugin     = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin     = require('copy-webpack-plugin');
 
 
 const prod = 'production';
@@ -15,56 +15,83 @@ const TARGET_ENV = process.env.npm_lifecycle_event === 'build' ? prod : dev;
 
 console.log('WEBPACK GO! Building for ' + TARGET_ENV);
 
+
+// used to extract CSS into a separate file (instead of
+// embedding inside JS)
+const extractCss = new ExtractTextPlugin({
+  filename: "[contenthash].css",
+  allChunks: true,
+  disable: TARGET_ENV === dev
+});
+
+
 // common webpack config (valid for dev and prod)
 var commonConfig = {
 
   output: {
-    path:     path.resolve( __dirname, 'dist/' ),
+    path:     path.resolve(__dirname, 'dist/'),
     filename: '[hash].js',
   },
 
   resolve: {
-    modules:            ['node_modules'],
-    extensions:         ['.js', '.elm']
+    modules:    ['node_modules'],
+    extensions: ['.js', '.elm']
   },
 
   module: {
     noParse: /\.elm$/,
-    rules: [{
-      test: /\.js$/,
-      exclude: ['/src/styles/', /node_modules/],
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['env']
+
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: ['/src/styles/', /node_modules/],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env']
+            }
+          },
+          {
+            loader: 'eslint-loader',
+            options: {
+              failOnWarning: true,
+              failOnError: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(css|less)$/,
+        use: extractCss.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'postcss-loader',
+            'less-loader'
+          ]
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|eot|ttf|woff|woff2|svg)$/,
+        use: {
+          loader: 'file-loader'
         }
       }
-    },
-    {
-      test: /\.(png|jpg|gif|eot|ttf|woff|woff2|svg)$/,
-      use: {
-        loader: 'file-loader'
-      }
-    }]
+    ]
   },
 
   plugins: [
+    extractCss,
+
+    new FaviconsWebpackPlugin('./src/favicon.png'),
+
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       inject:   'body',
       filename: 'index.html'
     }),
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        postcss: [
-          autoprefixer({browsers: ['last 2 versions']})
-        ],
-        eslint: {
-          failOnWarning: true,
-          failOnError: true
-        }
-      }
-    }),
+
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery"
@@ -84,7 +111,7 @@ if ( TARGET_ENV === dev ) {
     ],
 
     devServer: {
-      inline:   true,
+      inline: true,
       hot: true,
       historyApiFallback: true
     },
@@ -102,15 +129,6 @@ if ( TARGET_ENV === dev ) {
               debug: true
             }
           }]
-        },
-        {
-          test: /\.(css|less)$/,
-          use: [
-            'style-loader',
-            'css-loader',
-            'postcss-loader',
-            'less-loader'
-          ]
         }
       ]
     }
@@ -132,24 +150,11 @@ if ( TARGET_ENV === prod ) {
           test:    /\.elm$/,
           exclude: [/elm-stuff/, /node_modules/],
           use:  'elm-webpack-loader'
-        },
-        {
-          test: /\.(css|less)$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: ['css-loader', 'postcss-loader', 'less-loader']
-          })
         }
       ]
     },
 
     plugins: [
-      // extract CSS into a separate file
-      new ExtractTextPlugin({
-        filename: './[hash].css',
-        allChunks: true
-      }),
-
       new CopyWebpackPlugin([
         {
           from: 'src/images/',
@@ -158,9 +163,6 @@ if ( TARGET_ENV === prod ) {
         {
           from: 'src/papers/',
           to:   'papers/'
-        },
-        {
-          from: 'src/favicon.ico'
         }
       ]),
 
